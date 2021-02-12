4f84e72b5d5f65b92ec72500f75f5454d2a22c72
zaiatsFlagbit <zaiats85@mail.ru>
2021-02-12 03:39:07 +0100

    [lab-7] task 2 done

--- lab-7/index.js
+++ lab-7/index.js
@@ -1,6 +1,42 @@
 const endpoint = 'http://localhost:8080';
+const variant = 5;
 const modal = document.getElementById('id01');
 const result = document.getElementById('result');
+const table = document.getElementsByTagName("table");
+const td = table[0].getElementsByTagName("td");
+const colorPicker = document.getElementById('favcolor');
+
+td[variant - 1]
+    .addEventListener('mouseover', () => {
+        [...td].forEach((x, y) => {
+            if (y !== variant - 1) {
+                x.classList.add('w3-indigo');
+            }
+        })
+    });
+
+td[variant - 1]
+    .addEventListener('mouseout', () => {
+        [...td].forEach((x, y) => {
+            if (y !== variant - 1) {
+                x.classList.remove('w3-indigo');
+            }
+        })
+    });
+
+td[variant - 1].onclick = (event) => {
+    event.preventDefault();
+    colorPicker.click();
+};
+
+
+colorPicker.addEventListener('change', function(){
+    [...td].forEach((x, y) => {
+        if (y !== variant - 1) {
+            x.style.background = colorPicker.value;
+        }
+    })
+});
 
 let submitForm = (event) => {
     event.preventDefault();
@@ -20,6 +56,7 @@ let submitForm = (event) => {
         }
     )
 };
+
 
 // Пример отправки POST запроса:
 async function postData(url = '', data = {}) {
